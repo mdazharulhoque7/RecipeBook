0ef2b47cc5d226c893f81e51caacf89d1a7d5357
Md. Azharul Hoque <azharul.hoque@televersant.com>
2017-12-12 11:28:51 +0600

    10 Course Project - Services  Dependency Injection (Completed)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -14,7 +14,7 @@ export class RecipeService{
       [
         new Ingredient('Meat', 1),
         new Ingredient('French Fries', 20)
-      ]),git 
+      ]),
     new Recipe('Big Fat Burger',
       'What else you need to say?',
       'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
