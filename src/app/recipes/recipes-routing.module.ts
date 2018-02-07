ddcdccb65d81fe4b72cc47d4c41d775ca22f5ff9
Md. Azharul Hoque <azharul.hoque@televersant.com>
2018-02-07 19:31:13 +0600

    267 Creating a Shared Module

---
+++ src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,29 @@
+/**
+ * Created by azharul on 2/7/2018.
+ */
+
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+import {AuthGuard} from "../auth/auth-guard.service";
+
+import {RecipesComponent} from "./recipes.component";
+import {RecipeStartComponent} from "./recipe-start/recipe-start.component";
+import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
+import {RecipeDetailComponent} from "./recipe-detail/recipe-detail.component";
+
+const recipesRoute : Routes = [
+  {path:'recipes', component:RecipesComponent, children:[
+    {path:'', component: RecipeStartComponent},
+    {path:'new', component: RecipeEditComponent, canActivate:[AuthGuard]},
+    {path:':id', component: RecipeDetailComponent},
+    {path:':id/edit', component: RecipeEditComponent, canActivate:[AuthGuard]}
+  ]},
+];
+@NgModule({
+  imports:[
+    RouterModule.forChild(recipesRoute)
+  ],
+  exports:[RouterModule]
+})
+export class RecipesRoutingModule{}
